0bc566c2134f861d19503174f541c9768e83e1a3
Luis Alberto Sotelo Perafan <luis.sotelo@onelinkbpo.com>
2021-10-29 18:05:11 -0500

    Avance Curso JavaSpring

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,52 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private  ComprasProductoPk id;
+
+    private Integer cantidad;
+
+    private Double total;
+
+    private Boolean estado;
+
+    public ComprasProductoPk getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPk id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
